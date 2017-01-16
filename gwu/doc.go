d868c4f0c198a78d9b022c07659e9f07b9ec3bb4
Andras Belicza <iczaaa@gmail.com>
2017-01-16 14:31:42 +0100

    Added changes file for v1.3.0.

--- gwu/doc.go
+++ gwu/doc.go
@@ -396,7 +396,7 @@ package gwu
 
 // Gowut version information.
 const (
-	GowutVersion       = "v1.2.0-dev"     // Gowut version: "v"major.minor.maintenance[-dev]
+	GowutVersion       = "v1.3.0-dev"     // Gowut version: "v"major.minor.maintenance[-dev]
 	GowutReleaseDate   = "2017-01-16 CET" // Gowut release date
 	GowutRelDateLayout = "2006-01-02 MST" // Gowut release date layout (for time.Parse())
 )
