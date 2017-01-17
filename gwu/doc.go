9fac1ec629128932be7c51f1c36db38cd969eda6
Andras Belicza <iczaaa@gmail.com>
2017-01-17 16:59:55 +0100

    Gowut v1.2.2 public release.

--- gwu/doc.go
+++ gwu/doc.go
@@ -396,7 +396,7 @@ package gwu
 
 // Gowut version information.
 const (
-	GowutVersion       = "v1.2.2-dev"     // Gowut version: "v"major.minor.maintenance[-dev]
+	GowutVersion       = "v1.2.2"         // Gowut version: "v"major.minor.maintenance[-dev]
 	GowutReleaseDate   = "2017-01-17 CET" // Gowut release date
 	GowutRelDateLayout = "2006-01-02 MST" // Gowut release date layout (for time.Parse())
 )
